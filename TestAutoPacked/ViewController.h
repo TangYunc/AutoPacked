50e3b112df2f19d93bc2ee99c0f00d63f88d06f5
唐云川 <tangyunchuan@aumedia.com>
2017-03-01 11:41:03 +0800

    Initial Commit

---
+++ TestAutoPacked/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAutoPacked
+//
+//  Created by 唐云川 on 2017/3/1.
+//  Copyright © 2017年 com.guwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAutoPacked/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestAutoPacked
+//
+//  Created by 唐云川 on 2017/3/1.
+//  Copyright © 2017年 com.guwu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
